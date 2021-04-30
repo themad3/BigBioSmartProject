f941506365429b25f9c9d6e248c540eaa603758f
themad3 <83091328+themad3@users.noreply.github.com>
2021-04-30 13:11:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace The_Big_bio_project
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int ans;     
+
+
+
+
+
+            string name;     
+            Console.WriteLine("Hello, what is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ".");
+            Console.WriteLine("Here, let's wee how good you are at math.");
+            Console.WriteLine("Let's starts simple, what is 6x8?"); 
+            ans = Convert.ToInt32( Console.ReadLine() );
+
+            if (ans == 48){
+                Console.WriteLine("Correct!!");
+
+            }
+            else if (ans != 48) {
+                Console.WriteLine("Wrong!");
+            }
+
+
+
+
+             
+
+
+
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
